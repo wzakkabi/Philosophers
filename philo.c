cacb202181159f7eff5ee8c6f3af2a6155482a31
wzakkabi <walid1997e@gmail.com>
2023-06-19 08:32:19 +0100

    new one

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: wzakkabi <wzakkabi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 16:42:03 by wzakkabi          #+#    #+#             */
-/*   Updated: 2023/06/19 04:28:05 by wzakkabi         ###   ########.fr       */
+/*   Updated: 2023/06/19 08:17:31 by wzakkabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void ft_usleep(int t)
 	}
 }
 
-void full_the_format(int ac, char **av, struct philo_t *a)
+void full_the_format(int ac, char **av, philo_t *a)
 {
 	a->philo = atoi(av[1]);
 	a->die = atoi(av[2]);
@@ -51,7 +51,7 @@ void full_the_format(int ac, char **av, struct philo_t *a)
 		a->must_eat = 2147483647;
 }
 
-void	ft_eat(struct details_t *p)
+void	ft_eat(details_t *p)
 {
 	if(p->philo_number < p->test->philo)
 	{
@@ -63,8 +63,6 @@ void	ft_eat(struct details_t *p)
 		ft_usleep(p->test->eat);
 		pthread_mutex_unlock(&p->fork[p->philo_number - 1]);
 		pthread_mutex_unlock(&p->fork[p->philo_number]);
-		printf("%ld %d is sleeping\n", (ft_time_get() - p->time) , p->philo_number);
-		ft_usleep(p->test->sleep);
 	}
 	else
 	{
@@ -76,98 +74,82 @@ void	ft_eat(struct details_t *p)
 		ft_usleep(p->test->eat);
 		pthread_mutex_unlock(&p->fork[p->philo_number - 1]);
 		pthread_mutex_unlock(&p->fork[0]);
-		printf("%ld %d is sleeping\n", (ft_time_get() - p->time) , p->philo_number);
-		ft_usleep(p->test->sleep);
 	}
 }
 
 void *how_I_live(void *pp)
 {
-	struct details_t *p = (struct details_t *)pp;
+	details_t *p = (details_t *)pp;
 	while(p->meal < p->test->must_eat)
 	{
-		// if(p->philo_number % 2 == 0 && p->eat == 0)
-		// {
-		// 	ft_eat(p);
-		// 	p->eat = 1;
-		// }
-		// else
-		// {
-			printf("%ld %d is thinking\n", (ft_time_get() - p->time) , p->philo_number);
-			ft_eat(p);
-		//}
+		printf("%ld %d is thinking\n", (ft_time_get() - p->time) , p->philo_number);
+		ft_eat(p);
+		printf("%ld %d is sleeping\n", (ft_time_get() - p->time) , p->philo_number);
+		ft_usleep(p->test->sleep);
 		p->meal++;
 	}
 	return (void *)p;
 }
 
-
-void	philo(struct philo_t *a)
+void	creat_thread_modulo(philo_t *a, details_t *p)
 {
-	pthread_t philo[a->philo];
-	pthread_mutex_t fork[a->philo];
-	struct details_t p[a->philo];
-	int x = 0;
-	while(x < a->philo)
+	int	x;
+
+	a->tread = (pthread_t *)malloc(sizeof(pthread_t) * a->philo);
+	x = -1;
+	while (++x < a->philo)
 	{
-		if(pthread_mutex_init(&fork[x], NULL) != 0)
-		{
-			printf("Error mutex");
-			return ;
-		}
-		x++;
-	}
-	x = 0;
-	a->time = ft_time_get();
-	while(x < a->philo)
-	{
-		p[x].eat = 0;
-		p[x].philo_number = x + 1;
-		p[x].time = a->time;
-		p[x].fork = fork;
+		p[x].eat = ((p[x].philo_number = x + 1), 0);
+		p[x].time = ((p[x].fork = a->fork), a->time);
+		p[x].test = ((p[x].meal = 0), a);
 		p[x].last_meal = 0;
-		p[x].meal = 0;
-		p[x].test = a;
-		if(x % 2 == 0)
-		{
-			if(pthread_create(&philo[x], NULL, how_I_live, (void *)&p[x]) != 0)
-			{
-				printf("Error pthread");
+		if (x % 2 == 0)
+			if (pthread_create(&a->tread[x], NULL, how_I_live, (void *)&p[x]) != 0)
 				return ;
-			}
-		}
-		x++;
 	}
-	x = 0;
-	while(x < a->philo)
+	x = -1;
+	while (++x < a->philo)
 	{
-		if(x % 2 == 1)
-		{
-			if(pthread_create(&philo[x], NULL, how_I_live, (void *)&p[x]) != 0)
-			{
-				printf("Error pthread");
+		if (x % 2 == 1)
+			if (pthread_create(&a->tread[x], NULL, how_I_live, (void *)&p[x]) != 0)
 				return ;
-			}
-		}
-		x++;
 	}
+}
+
+// //p = (struct details_t *)malloc(sizeof(struct details_t) * a->philo);
+void	philo(philo_t *a)
+{
+	pthread_mutex_t		*fork;
+	details_t	*p;
+	int					x;
+
+	fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * a->philo);
+	p = (details_t *)malloc(sizeof(details_t) * a->philo);
+	x = -1;
+	while (++x < a->philo)
+		if (pthread_mutex_init(&fork[x], NULL) != 0)
+			return ;
+	a->fork = fork;
+	a->time = ft_time_get();
+	creat_thread_modulo(a, p);
 	x = 0;
 	ft_usleep(a->die);
-	while(1)
+	while (1)
 	{
-		if(ft_time_get() - p[x].last_meal > a->die)
+		if (ft_time_get() - p[x].last_meal > a->die)
 		{
-			printf("%ld %d died\n", (ft_time_get() - p[x].time),  p->philo_number);
+			printf("%ld %d died\n",
+				(ft_time_get() - p[x].time), p->philo_number);
 			return ;
 		}
-		if(x == a->philo)
+		if (x == a->philo)
 			x = 0;
 	}
 }
 
 int main(int ac, char **av)
 {
-	struct philo_t a;
+	philo_t a;
 	int x = 0, y = 1;
 	if(ac == 5 || ac == 6)
 	{
--- philo.h
+++ philo.h
@@ -6,46 +6,47 @@
 /*   By: wzakkabi <wzakkabi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 22:42:13 by wzakkabi          #+#    #+#             */
-/*   Updated: 2023/06/19 03:53:34 by wzakkabi         ###   ########.fr       */
+/*   Updated: 2023/06/19 07:29:24 by wzakkabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <string.h>
-#include <pthread.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <string.h>
+# include <pthread.h>
+# include <stdlib.h>
 
-struct philo_t
+typedef struct s_details details_t;
+typedef struct s_philo
 {
-	int philo;
-	int die;
-	int eat;
-	int sleep;
-	int must_eat;
-	int die_or_not;
-	long int time;
-	struct details_t *dtls;
-};
+	int				philo;
+	int				die;
+	int				eat;
+	int				sleep;
+	int				must_eat;
+	int				die_or_not;
+	long int		time;
+	details_t		*dtls;
+	pthread_t		*tread;
+	pthread_mutex_t	*fork;
+}philo_t;
 
-struct details_t
+typedef struct s_details
 {
-	int philo_number;
-	long int time;
-	long int last_meal;
-	int meal;
-	struct philo_t *test;
-	pthread_mutex_t *fork;
-	int eat;
-};
+	int				philo_number;
+	long int		time;
+	long int		last_meal;
+	int				meal;
+	philo_t			*test;
+	pthread_mutex_t	*fork;
+	int				eat;
+}details_t;
 
-void	ft_eat(struct details_t *p);
-
-
+//void	ft_eat(struct details_t *p);
 
 #endif
